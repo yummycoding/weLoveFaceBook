89853625894f0f011d2243e2d3eddb51bdf56b0b
wobusimanong <269410834@qq.com>
2017-11-03 20:48:39 -0400

    test codes

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -133,7 +133,115 @@ router.delete('/deleteBlog/:id', (req, res) => {
                     })
                 }
             }
-        })
+        });
+    }
+});
+
+router.put('/likePost', (req, res) => {
+    if (!req.body.id) {
+        res.json({success: false, message: 'No id was provided'});
+    } else {
+        Post.findOne({_id: req.body.id}, (err, post) => {
+            if (err) {
+                res.json({success: false, message: 'Invalid post id'});
+            } else {
+                User.findOne({_id: req.decode.userId}, (err, user) => {
+                    if (err) {
+                        res.json({success: false, message: 'Something went wrong'});
+                    } else {
+                        if (!user) {
+                            res.json({success: false, message: 'Could not authenticate user.'});
+                        } else {
+                            if (user.username === post.createdBy) {
+                                res.json({success: false, message: 'Cannot like your own post.'});
+                            } else {
+                                if (post.likedBy.includes(user.username)) {
+                                    res.json({success: false, message: 'You already liked this post.'});
+                                } else {
+                                    if (post.dislikedBy.includes(user.username)) {
+                                        post.dislikedBy.splice(arrayIndex, 1);
+                                        post.likes++;
+                                        post.likedBy.push(user.username);
+                                        post.save((err) => {
+                                            if (err) {
+                                                res.json({success: false, message: 'Something went wrong.'});
+                                            } else {
+                                                res.json({success: true, message: 'Post liked!'});
+                                            }
+                                        });
+                                    } else {
+                                        post.likes++;
+                                        post.likedBy.push(user.username);
+                                        post.save((err) => {
+                                            if (err) {
+                                                res.json({success: false, message: 'Something went wrong'});
+                                            } else {
+                                                res.json({success: true, message: 'Post liked!'});
+                                            }
+                                        });
+                                    }
+                                }
+                            }
+                        }
+                    }
+                });
+            }
+        });
+    }
+});
+
+router.put('/dislikePost', (req, res) => {
+    if (!req.body.id) {
+        res.json({success: false, message: 'No id was provided.'});
+    } else {
+        Post.findOne({_id: req.body.id}, (err, blog) => {
+            if (err) {
+                res.json({success: false, message: 'Invalid post id'});
+            } else {
+                if (!post) {
+                    res.json({success: false, message: 'That post was not found'});
+                } else {
+                    User.findOne({_id: req.decoded.userId}, (err, user) => {
+                        if (err) {
+                            res.json({success: false, message: 'Something went wrong.'});
+                        } else {
+                            if (!user) {
+                                res.json({success: false, message: 'Could not authenticate user.'});
+                            } else {
+                                if (user.username === post.createdBy) {
+                                    res.json({success: false, message: 'You already disliked this post'});
+                                } else {
+                                    if (post.likedBy.includes(user.username)) {
+                                        post.likes--;
+                                        const arrayIndex = post.likedBy.indexOf(user.username);
+                                        post.likedBy.splice(arrayIndex, 1);
+                                        post.dislikes++;
+                                        post.dislikedBy.push(user.username);
+                                        post.save((err) => {
+                                            if (err) {
+                                                res.json({success: false, message: 'Something went wrong'});
+                                            } else {
+                                                res.json({success: true, message: 'Post disliked!'});
+                                            }
+                                        });
+                                    } else {
+                                        post.dislikes++;
+                                        post.dislikedBy.push(user.username);
+                                        post.save((err) => {
+                                            if (err) {
+                                                res.json({success: false, message: 'Something went wrong.'});
+                                            } else {
+                                                res.json({success: true, message: 'Post disliked!'});
+                                            }
+                                        });
+                                    }
+                                }
+                            }
+                        }
+                    });
+                }
+            }
+        });
     }
 })
 
--- backend/test/postTest.js
+++ backend/test/postTest.js
@@ -1,3 +1,11 @@
+var mongoose = require('mongoose');
+var chai = require('chai');
 var expect  = require("chai").expect;
 var request = require("request");
 var Post = require('../models/post');
+
+describe('Test the APIs in post', function() {
+    it('Title Length checker should return false', function(done){
+       
+    })
+})
