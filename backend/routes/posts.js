073362b13a2f62f52618474608b8cf1ec7c84c46
wobusimanong <269410834@qq.com>
2017-11-06 14:12:29 -0500

    delete

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -155,33 +155,8 @@ router.put('/updatePost', (req, res) => {
     }
 });
 
-// router.delete('/deletePost/:id', (req, res) => { 
-//     console.log('DELETE > /deletePost/:id > id', req.params.id);
-//     if (!req.params.id) {
-//         res.json({success: false, message: 'No id provided'});
-//     } else {
-//         Post.findOne({_id: req.params.id}, (err, post) => {
-//             if (err) {
-//                 res.json({success: false, message: 'Invalid id'});
-//             } else {
-//                 if (!post) {
-//                     res.json({success: false, message: 'Post was not found'});
-//                 } else {
-//                     post.remove((err) => {
-//                         if (err) {
-//                             res.json({success: false, message: err});
-//                         } else {
-//                             res.json({success: true, message: 'Post deleted successfully!'});
-//                         }
-//                     })
-//                 }
-//             }
-//         });
-//     };
-// });
-
 router.delete('/deletePost/:id', (req, res) => { 
-    console.log('DELETE > /deleteBlog/:id > id', req.params.id);
+    console.log('DELETE > /deletePost/:id > id', req.params.id);
     if (!req.params.id) {
         res.json({success: false, message: 'No id provided'});
     } else {
@@ -192,32 +167,19 @@ router.delete('/deletePost/:id', (req, res) => {
                 if (!post) {
                     res.json({success: false, message: 'Post was not found'});
                 } else {
-                    User.findOne({_id: req.decoded.userId}, (err, user) => {
+                    post.remove((err) => {
                         if (err) {
-                           res.json({success: false, message: err});
+                            res.json({success: false, message: err});
                         } else {
-                            if (!user) {
-                                res.json({success: false, message: 'Unable to authenticate user'});
-                            } else {
-                                if (user.username !== post.createdBy) {
-                                    res.json({success: false, message: 'You are not authorized to delete this post'});
-                                } else {
-                                    post.remove((err) => {
-                                        if (err) {
-                                            res.json({success: false, message: err});
-                                        } else {
-                                            res.json({success: true, message: 'Post deleted successfully!'});
-                                        }
-                                    })
-                                }
-                            }
+                            res.json({success: true, message: 'Post deleted successfully!'});
                         }
                     })
                 }
             }
         });
-    }
+    };
 });
+
 
 router.put('/likePost', (req, res) => {
     if (!req.body.id) {
@@ -347,7 +309,7 @@ router.post('/comment', (req, res) => {
               res.json({ success: false, message: 'Post not found.' }); // Return error message
             } else {
               // Grab data of user that is logged in
-              User.findOne({ _id: req.decoded.userId }, (err, user) => {
+              User.findOne({ _id: req.body.id }, (err, user) => {
                 // Check if error was found
                 if (err) {
                   res.json({ success: false, message: 'Something went wrong' }); // Return error message
